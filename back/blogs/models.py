0c7784e955328470ddf2c235b082d6003d8b09a7
mintropy <jun2korea@gmail.com>
2022-09-02 23:52:20 +0900

    [back] models created & migration files

--- back/blogs/models.py
+++ back/blogs/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+from users.models import User
+
 # Create your models here.
+class Blog(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, related_name="blogs", on_delete=models.PROTECT)
+    name = models.CharField(max_length=20)
+
+
+class BlogPost(models.Model):
+    id = models.AutoField(primary_key=True)
+    blog = models.ForeignKey(Blog, related_name="posts", on_delete=models.PROTECT)
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
