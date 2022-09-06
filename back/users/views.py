38309e80463430edb147fa9cafe11264413f60c3
mintropy <jun2korea@gmail.com>
2022-09-06 23:09:23 +0900

    [back] user view update

--- back/users/views.py
+++ back/users/views.py
@@ -3,28 +3,39 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.views import status
 
 from .models import User
+from .serializers import UserSerializer
 
 
 # Create your views here.
 class UserViewSet(ViewSet):
-    model = User
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
 
     def signup(self, request):
-        data = {"username": "admin"}
-        return Response(data=data, status=status.HTTP_201_CREATED)
+        data = {
+            "username": request.data.get("username", ""),
+            "password": request.data.get("password", ""),
+        }
+        serialzier = UserSerializer(data=data)
+        if serialzier.is_valid():
+            serialzier.save()
+            return Response(data=serialzier.data, status=status.HTTP_201_CREATED)
+        return Response(data=serialzier.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def retrieve_profile(self, request):
-        data = {
-            "username": "admin",
-            "email": "smaple@smaple.com",
-            "blogs": ["sample blog name"],
-        }
-        return Response(data=data, status=status.HTTP_200_OK)
+        user = User.objects.get(username="admin")
+        serializer = UserSerializer(instance=user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def update_profile(self, request):
+        user = User.objects.get(username="admin")
         data = {
-            "username": "admin",
-            "email": "smaple@smaple.com",
-            "blogs": ["sample blog name"],
+            "username": user.username,
+            "password": user.password,
+            "email": request.data.get("email", ""),
         }
-        return Response(data=data, status=status.HTTP_201_CREATED)
+        serializer = UserSerializer(instance=user, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
