bd2020a336df03288fdec991b2dd12909c06269d
mintropy <jun2korea@gmail.com>
2022-09-21 15:07:01 +0900

    [front-error] fecth test, CORS

--- front/src/Main/MainBlogList.js
+++ front/src/Main/MainBlogList.js
@@ -1,7 +1,11 @@
 import React from "react";
-import "./Main.css"
+import "./Main.css";
 
 const MainBlogList = () => {
+  fetch("http://127.0.0.1:8000/api/blogs/blog/").then((res) =>
+    console.log(res)
+  );
+
   return (
     <div className="MainBlogList">
       <h1>블로그 목록</h1>
