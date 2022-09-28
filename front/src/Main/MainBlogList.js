69dc15226be66afe2fadbc7eccbacc4560559d06
mintropy <jun2korea@gmail.com>
2022-09-28 17:14:06 +0900

    [front-error] api setting node upate axios react useState & useEffect error

--- front/src/Main/MainBlogList.js
+++ front/src/Main/MainBlogList.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./Main.css";
 
 const MainBlogList = () => {
-  fetch("http://127.0.0.1:8000/api/blogs/blog/").then((res) =>
-    console.log(res)
-  );
+  const [blogs, setBlogs] = useState();
+  const getBlogs = async () => {
+    await axios({
+      method: "get",
+      url: "http://127.0.0.1:8000/api/blogs/blog/",
+    })
+      .then((res) => setBlogs(res.data))
+      .catch((err) => []);
+  };
+  useEffect(() => {
+    getBlogs();
+  }, []);
 
   return (
     <div className="MainBlogList">
       <h1>블로그 목록</h1>
+      <ul>
+        {console.log(blogs)}
+        {/* {blogs.map((item) => (
+          <li key={item.objectID}>{item.title}</li>
+        ))} */}
+      </ul>
     </div>
   );
 };
