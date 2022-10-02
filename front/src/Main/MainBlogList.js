f449ed4872ce0cc6e85041ba496416ac7e321ce6
mintropy <jun2korea@gmail.com>
2022-10-02 18:44:17 +0900

    [front] solve #10, render blog list in main page

--- front/src/Main/MainBlogList.js
+++ front/src/Main/MainBlogList.js
@@ -3,14 +3,13 @@ import axios from "axios";
 import "./Main.css";
 
 const MainBlogList = () => {
-  const [blogs, setBlogs] = useState();
+  const [blogs, setBlogs] = useState([]);
   const getBlogs = async () => {
-    await axios({
+    const res = await axios({
       method: "get",
       url: "http://127.0.0.1:8000/api/blogs/blog/",
-    })
-      .then((res) => setBlogs(res.data))
-      .catch((err) => []);
+    }).then((res) => res.data);
+    setBlogs(res);
   };
   useEffect(() => {
     getBlogs();
@@ -20,10 +19,9 @@ const MainBlogList = () => {
     <div className="MainBlogList">
       <h1>블로그 목록</h1>
       <ul>
-        {console.log(blogs)}
-        {/* {blogs.map((item) => (
-          <li key={item.objectID}>{item.title}</li>
-        ))} */}
+        {blogs.map((item) => (
+          <li key={item.id}>{item.name}</li>
+        ))}
       </ul>
     </div>
   );
