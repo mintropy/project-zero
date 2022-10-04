64004450809a88d767c614eccb7c28da237b8095
mintropy <jun2korea@gmail.com>
2022-10-04 10:58:15 +0900

    [front] #6 header update

--- front/src/Header.js
+++ front/src/Header.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./index.css";
 
 const Header = () => {
   return (
     <div className="Header">
-      <h1>Project Zero</h1>
-      <Link to={"/"}>Main</Link>
-      <br />
+      <Link to={"/"}>Zero</Link>
       <Link to={"/blog/"}>Blog</Link>
-      <br />
       <Link to={"/account/"}>account</Link>
     </div>
   );
